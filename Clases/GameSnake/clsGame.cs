4cbe1aaa1c3b53bc61f967d975fdf7ab2c85027a
Erick <evillanuevag1@miumg.edu.gt>
2021-05-17 17:35:35 -0600

    Agregué la clase clsGame que será la clase padre, clsGameSnake es donde vamos a tener el código original

---
+++ Clases/GameSnake/clsGame.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Dayana_Erickson.Clases.GameSnake
+{
+    class clsGame
+    {
+        internal enum Direction
+        {
+            Abajo, Izquierda, Derecha, Arriba
+        }
+        private void DibujaPantalla(Size size)
+        {
+            Console.Title = "Culebrita comelona";
+            Console.WindowHeight = size.Height + 2;
+            Console.WindowWidth = size.Width + 2;
+            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferWidth = Console.WindowWidth;
+            Console.CursorVisible = false;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            for (int row = 0; row < size.Height; row++)
+            {
+                for (int col = 0; col < size.Width; col++)
+                {
+                    Console.SetCursorPosition(col + 1, row + 1);
+                    Console.Write(" ");
+                }
+            }
+        }//end DibujaPantalla
+
+        private void MuestraPunteo(int punteo)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.SetCursorPosition(1, 0);
+            Console.Write($"Puntuación: {punteo.ToString("00000000")}");
+        }//end Muestra Punteo
+
+        private Direction ObtieneDireccion(Direction direccionAcutal)
+        {
+            if (!Console.KeyAvailable) return direccionAcutal;
+
+            var tecla = Console.ReadKey(true).Key;
+            switch (tecla)
+            {
+                case ConsoleKey.DownArrow:
+                    if (direccionAcutal != Direction.Arriba)
+                        direccionAcutal = Direction.Abajo;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    if (direccionAcutal != Direction.Derecha)
+                        direccionAcutal = Direction.Izquierda;
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (direccionAcutal != Direction.Izquierda)
+                        direccionAcutal = Direction.Derecha;
+                    break;
+                case ConsoleKey.UpArrow:
+                    if (direccionAcutal != Direction.Abajo)
+                        direccionAcutal = Direction.Arriba;
+                    break;
+            }
+            return direccionAcutal;
+        }//end obtieneDireccion
+
+        private Point ObtieneSiguienteDireccion(Direction direction, Point currentPosition)
+        {
+            Point siguienteDireccion = new Point(currentPosition.X, currentPosition.Y);
+            switch (direction)
+            {
+                case Direction.Arriba:
+                    siguienteDireccion.Y--;
+                    break;
+                case Direction.Izquierda:
+                    siguienteDireccion.X--;
+                    break;
+                case Direction.Abajo:
+                    siguienteDireccion.Y++;
+                    break;
+                case Direction.Derecha:
+                    siguienteDireccion.X++;
+                    break;
+            }
+            return siguienteDireccion;
+        }// end ObtieneSiguienteDireccion
+    }
+}
